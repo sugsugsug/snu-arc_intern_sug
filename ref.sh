0be5831ef3ef20ee6941d8b8bc1f819bd93d1eb4
sugsugsug <thddnrud2010@gmail.com>
2019-07-24 21:12:20 +0900

    ref

---
+++ ref.sh
@@ -0,0 +1,17 @@
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf.idx ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz.tbi ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.dict ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.fai ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.alt ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.sa ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.amb ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.bwt ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.ann ./inputs/references/ &
+gsutil cp gs://broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.pac ./inputs/references/ &
+
+
