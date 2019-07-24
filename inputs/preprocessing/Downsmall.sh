82d9ad4ec0cf8abe594d7be5df96ac120c2c0069
sugsugsug <thddnrud2010@gmail.com>
2019-07-24 21:14:38 +0900

    smalldown

---
+++ inputs/preprocessing/Downsmall.sh
@@ -0,0 +1,27 @@
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HJYFJ.4.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HJYFJ.5.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HJYFJ.6.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HJYFJ.7.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HJYFJ.8.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HJYN2.1.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.1.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.2.NA12878.downsampled.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.3.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.4.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.5.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.6.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.7.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35M.8.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35N.1.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK35N.2.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.1.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.2.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.3.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.4.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.5.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.6.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.7.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/HK3T5.8.NA12878.interval.filtered.query.sorted.unmapped.bam &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/small/smalltest.files &
+wget https://storage.googleapis.com/gatk-test-data/wgs_ubam/NA12878_24RG/NA12878_24RG_small.txt &
+
