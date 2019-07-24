bb69889c6b5fb08538337f960538f731b843871a
sugsugsug <thddnrud2010@gmail.com>
2019-07-24 20:26:26 +0900

    lss

---
+++ LocalTool/surfaceTime/mktimetable.sh
@@ -0,0 +1,13 @@
+java -jar ../../ThirdJAVASumChipTime.jar start REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar getbwaversion.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar samtofastq.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar bwamem.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar samtools.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar mergebamalignment.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar Sort.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar FixTags.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar markduplicates.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar baserecalibrator.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar gatherreports.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar applybqsr.txt REsult.txt 8
+java -jar ../../ThirdJAVASumChipTime.jar gatherbamfiles.txt REsult.txt 8
